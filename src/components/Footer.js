e7aedeb24f37fd8a1272cb40cbe6effd54323ae4
Uche <uchaynwankwo@gmail.com>
2022-11-03 19:14:58 +0100

    fix: make the web page mobile responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import zuri from '../images/zuri-logo.png';
 
 const Footer = () => {
   return (
-    <div className='footer block mt-5 mb-8 mx-5 md:mx-28 py-6 md:pt-6 md:pb-4'>
+    <div className='footer block mt-5 mb-20 md:mb-8 mx-5 md:mx-28 py-6 md:pt-6 md:pb-4'>
         <div className='flex flex-col md:flex-row md:justify-between items-start md:items-end gap-4 md:gap-0 h-16'>
             <img src={zuri} alt='Zuri Logo'/>
             <span className='footer-text'>HNG Internship 9 Frontend Task</span>
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,25 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Contact = () => {
+  const [success, setSuccess] =useState(false);
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    alert(window.innerHeight);
+    setSuccess(true);
+    setTimeout(() => {
+      setSuccess(false);
+    }, 5000);
   }
   return (
-    <div className='contact flex justify-center pt-24 pb-28 px-12'>
+    <div className='contact flex justify-center pt-16 md:pt-24 pb-20 md:pb-28 px-4 md:px-12'>
       <div className='contact-details'>
         <section className='contact-header w-fit'>
           <h2 className='text-4xl font-semibold my-3'>Contact Me</h2>
           <p className='text-xl my-3'>Hi there, contact me to ask me about anything you have in mind.</p>
         </section>
-        <form onSubmit={handleSubmit} className='w-full my-6'>
-          <div className='flex py-3 gap-6'>
-              <span className='w-1/2'>
+        <form onSubmit={handleSubmit} className='w-full my-6  relative'>
+          <div className='flex flex-col md:flex-row md:py-3 md:gap-6'>
+              <span className='md:w-1/2 py-3 md:py-0'>
                 <label htmlFor='first_name' className='label'>First name</label>
                 <input type='text' id='first_name' name='first_name' placeholder='Enter your first name' />
               </span>
-              <span className='w-1/2'>
+              <span className='md:w-1/2 py-3 md:py-0'>
                 <label htmlFor='last_name' className='label'>Last name</label>
                 <input type='text' id='last_name' name='last_name' placeholder='Enter your last name' />
               </span>
@@ -32,11 +36,12 @@ const Contact = () => {
               <label htmlFor='message' className='label'>Message</label>
               <textarea id='message' name='message' placeholder="Send me a message and I'll reply you as soon as possible..." rows='5' className='w-full'/>
           </div>
-          <div className='py-3 flex items-center'>
-              <input type='checkbox' id='agreement' className='mr-3'/>
+          <div className='py-3 flex md:items-center'>
+              <input type='checkbox' id='agreement' className='mr-3 mt-1 md:mt-0'/>
               <label htmlFor='agreement' style={{color: '#475467'}}>You agree to providing your data to Uche who may contact you.</label>
           </div>
           <button id='btn__submit' className='contact-submit text-base font-semibold text-white w-full py-3 my-4' type='submit'>Send Message</button>
+          <p className={success ? 'success-msg text-sm text-green-500 font-semibold text-center absolute' : 'hidden'}>Message Sent</p>
         </form>
       </div>
     </div>
